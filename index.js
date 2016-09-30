6f052574ad89c67b4b4cad66f9e3e47c95f71bc3
Demon <demonsingur@gmail.com>
2016-09-30 15:58:31 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        ipaddress: req.headers['x-forwarded-for'],
+        language: req.headers['accept-language'].split(',')[0],
+        software: req.headers['user-agent'].split('(')[1].split(')')[0]
+    });
+});
+
+app.listen(process.env.PORT);
